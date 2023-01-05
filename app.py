4b9b6f6d2a46c9039ac9dac9b078cbca6bcdb91d
ziro <palembani@gmail.com>
2023-01-05 07:54:23 +0700

    refactor(v1.0.0): v1 prefix

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 
 from routers import google, imagemanip
 
+PREFIX = "/api/v1"
+
 app = FastAPI()
-app.include_router(google.router)
-app.include_router(imagemanip.router)
+app.include_router(google.router, prefix=PREFIX)
+app.include_router(imagemanip.router, prefix=f"{PREFIX}/image")
--- build-and-push.sh
+++ build-and-push.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-sudo docker build -t ghcr.io/ziro-bot/randomapi . \
-&& sudo docker push ghcr.io/ziro-bot/randomapi:latest
+sudo docker build -t ghcr.io/ziro-bot/randomapi -t ghcr.io/ziro-bot/randomapi:1.0.0 . \
+&& sudo docker push ghcr.io/ziro-bot/randomapi:latest \
+&& sudo docker push ghcr.io/ziro-bot/randomapi:1.0.0
