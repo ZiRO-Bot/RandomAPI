fac66e5e584bd7e311b4fbc764a29dbb79bf8311
Ahmad Ansori Palembani <46041660+null2264@users.noreply.github.com>
2023-07-09 13:48:24 +0700

    chore: Change default port to 8000

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import uvicorn
 
 if __name__ == "__main__":
-    port: int = 2264
+    port: int = 8000
     config = uvicorn.Config("app:app", port=port, host="0.0.0.0")
 
     runner = uvicorn.Server(config)
